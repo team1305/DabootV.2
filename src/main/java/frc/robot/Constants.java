bfb1cad38b4dda9bfbe4130deac968ee178a34e9
Programming Laptop <programming@team1305.org>
2022-02-02 19:37:35 -0500

    :\

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,6 +19,8 @@ import edu.wpi.first.wpilibj.Solenoid;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+
+ //:)
 public final class Constants {
 
 
