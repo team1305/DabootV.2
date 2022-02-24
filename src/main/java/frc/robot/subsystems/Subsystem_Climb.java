3304384d4d97c9ad325b513512de6ac0a6d21c90
Programming Laptop <programming@team1305.org>
2022-02-23 20:49:08 -0500

    adds climb loop command

--- src/main/java/frc/robot/subsystems/Subsystem_Climb.java
+++ src/main/java/frc/robot/subsystems/Subsystem_Climb.java
@@ -8,6 +8,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.commands.Command_Climb_Loop;
 
 
 public class Subsystem_Climb extends SubsystemBase {
@@ -15,7 +17,7 @@ public class Subsystem_Climb extends SubsystemBase {
  private final static WPI_TalonFX mtClimb2 = Constants.mtClimb2;
  private final static Solenoid slndClimb1 = Constants.slndClimb;
 
- 
+ private Integer idpad;
 
   /** Creates a new Subsystem_Climb. */
   public Subsystem_Climb() {
@@ -30,6 +32,25 @@ public class Subsystem_Climb extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    
+    setDefaultCommand(new Command_Climb_Loop(RobotContainer.Climb));
+
+  }
+
+  public void Climb_Loop() {
+     idpad = RobotContainer.getJoystickOperator().getPOV();
+
+    if (idpad == 0) {
+      // Climb Up
+      ClimbUp(0.75);
+    } else if (idpad == 180) {
+      // Climb Down
+      ClimbDown(-0.75);
+    } else {
+      // anything else
+      stopCLimb(0);
+    }
+
   }
 
   public void HighGear() {
@@ -40,6 +61,18 @@ public class Subsystem_Climb extends SubsystemBase {
   public void LowGear() {
     slndClimb1.set(false);
 
+  }
+
+  public void ClimbUp(double speed) {
+    HighGear();
+    mtClimb1.set(speed);
+    mtClimb2.set(speed);
+  }
+  
+  public void ClimbDown(double speed) {
+    LowGear();
+    mtClimb1.set(speed);
+    mtClimb2.set(speed);
   }
 
   public void startClimb(double speed) {
